4126ff23dfdeeb08537ebdd2071b2d8d5bb24d75
Connor Vosberg <connorjv@gmail.com>
2023-10-14 16:02:29 -0700

    commit tree 970998b084340c769b7e2ef8b944daf2f3797ee2

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,11 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case 'friends/add':
+      return { ...state, friends: [...state.friends, action.payload] }
+    case 'friends/remove':
+      const updatedFriends = state.friends.filter(friend => friend.id !== action.payload)
+      return {...state, friends: updatedFriends}
+    default: 
+      return state
+  }
 }
